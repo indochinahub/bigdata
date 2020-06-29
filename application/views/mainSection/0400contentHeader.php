797d67c5ed9047dc12750bd2bdd71b7cb128f945
Wittaya Wijit <indochinahub@gmail.com>
2020-06-29 16:10:20 +0700

    ปรับโครงสร้างแฟ้ม เพื่อเตรียมแก้ไข, ดูความเรียบร้อยใน Controller : Main

--- application/views/mainSection/0400contentHeader.php
+++ application/views/mainSection/0400contentHeader.php
@@ -4,17 +4,6 @@
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Bigdata</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="<?php echo base_url();?>">Home</a></li>
-              <li class="breadcrumb-item active">current page</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
