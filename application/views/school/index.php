b84574f5aa0dbc10e54e97328a37b29fca595835
Wittaya Wijit <indochinahub@gmail.com>
2020-07-01 10:50:13 +0700

    แก้ไขวิว  school/index.php

--- application/views/school/index.php
+++ application/views/school/index.php
@@ -1,46 +1,28 @@
-<!-- Small boxes (Stat box) -->
-<div class="row">
-
-    <div class="col-lg-3 col-6">
-        <h2>โรงเรียนในสังกัด</h2>
-        <p>
-            <?php foreach( $schools as $school){ } ?>
-        </p>
-
+<div>
+    <div style="margin:10px 0 0 10px;text-align:center">
+        <h3>สถานศึกษาในสังกัด</h3>
     </div>
 
-    <div class="col-12">
-          <div class="card">
-            <div class="card-body">
-              <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4"><div class="row"><div class="col-sm-12 col-md-6"></div><div class="col-sm-12 col-md-6"></div></div><div class="row"><div class="col-sm-12"><table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
-                <thead>
-                <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">ชื่อโรงเรียน</th>
-                    <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">รหัสกระทรวง</th>
-                    <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">รหัส SMISS</th>
-                </tr>
-                </thead>
-                <tbody>
-                    <?php foreach( $schools as $school){ ?>
-                        <tr role="row" class="odd">
-                            <td><a href="<?php echo base_url(["school", "showSchoolDetailView", $school->school_id]);?>"><?php echo $school->school_thainame;?></a></td>
-                            <td><?php echo $school->school_id;?></td>
-                            <td><?php echo $school->school_smisid;?></td>
-                        </tr>
-                    <?php } ?>
-            </tbody>
-              </table>
-            
-            </div>
-            <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
-
-
-    </div>
-
-
+    <div style="margin:20px 0 0 10px;color:#28a745">
+        <h4>[ รายชื่อโรงเรียน ] </h4>
+    </div>    
     
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">ชื่อโรงเรียน</th>
+                <th scope="col">รหัสกระทรวง</th>
+                <th scope="col">รหัส SMISS</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach( $schools as $school){ ?>
+                <tr>
+                    <th scope="row"><a href="<?php echo base_url(["school", "showSchoolDetailView", $school->school_id]);?>"><?php echo $school->school_thainame;?></a></th>
+                    <td><?php echo $school->school_id;?></td>
+                    <td><?php echo $school->school_smisid;?></td>
+                </tr>
+            <?php } ?>            
+        </tbody>
+    </table>
 </div>
-<!-- /.row -->
-
-            
